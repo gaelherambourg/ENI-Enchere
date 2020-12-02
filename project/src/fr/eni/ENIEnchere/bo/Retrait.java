af4ddf3cb3494db45d09f14c7119996118eeedf0
gaelherambourg <75248829+gaelherambourg@users.noreply.github.com>
2020-12-02 09:55:21 +0100

    Classes Bo

--- project/src/fr/eni/ENIEnchere/bo/Retrait.java
+++ project/src/fr/eni/ENIEnchere/bo/Retrait.java
@@ -38,39 +38,85 @@ public class Retrait {
 		this.ville = ville;
 	}
 
+	
+	/**
+	 * @return the article
+	 */
 	public Article getArticle() {
 		return article;
 	}
+
 	
+	/**
+	 * @param article the article to set
+	 */
 	public void setArticle(Article article) {
 		this.article = article;
 	}
+
 	
+	/**
+	 * @return the rue
+	 */
 	public String getRue() {
 		return rue;
 	}
+
 	
+	/**
+	 * @param rue the rue to set
+	 */
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
+
 	
+	/**
+	 * @return the code_postal
+	 */
 	public String getCode_postal() {
 		return code_postal;
 	}
+
 	
+	/**
+	 * @param code_postal the code_postal to set
+	 */
 	public void setCode_postal(String code_postal) {
 		this.code_postal = code_postal;
 	}
+
 	
+	/**
+	 * @return the ville
+	 */
 	public String getVille() {
 		return ville;
 	}
+
 	
+	/**
+	 * @param ville the ville to set
+	 */
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
+
 	
-	
-	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Retrait [article=");
+		builder.append(article);
+		builder.append(", rue=");
+		builder.append(rue);
+		builder.append(", code_postal=");
+		builder.append(code_postal);
+		builder.append(", ville=");
+		builder.append(ville);
+		builder.append("]");
+		return builder.toString();
+	}
+
 	
 }
--- project/src/fr/eni/ENIEnchere/bo/Utilisateur.java
+++ project/src/fr/eni/ENIEnchere/bo/Utilisateur.java
@@ -24,16 +24,12 @@ public class Utilisateur implements Serializable {
 	private boolean administrateur;
 	
 	
-	
-	
 	/**
 	 * 
 	 */
 	public Utilisateur() {
 		super();
 	}
-	
-	
 	
 	/**
 	 * @param pseudo
@@ -95,7 +91,6 @@ public class Utilisateur implements Serializable {
 		this.administrateur = administrateur;
 	}
 
-	
 	/**
 	 * @param pseudo
 	 * @param nom
@@ -123,77 +118,98 @@ public class Utilisateur implements Serializable {
 		this.administrateur = administrateur;
 	}
 
-
-
 	public int getNo_utilisateur() {
 		return no_utilisateur;
 	}
+	
 	public void setNo_utilisateur(int no_utilisateur) {
 		this.no_utilisateur = no_utilisateur;
 	}
+	
 	public String getPseudo() {
 		return pseudo;
 	}
+	
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
 	public String getPrenom() {
 		return prenom;
 	}
+	
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getTelephone() {
 		return telephone;
 	}
+	
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+	
 	public String getRue() {
 		return rue;
 	}
+	
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
+	
 	public String getCode_postal() {
 		return code_postal;
 	}
+	
 	public void setCode_postal(String code_postal) {
 		this.code_postal = code_postal;
 	}
+	
 	public String getVille() {
 		return ville;
 	}
+	
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
+	
 	public String getMot_de_passe() {
 		return mot_de_passe;
 	}
+	
 	public void setMot_de_passe(String mot_de_passe) {
 		this.mot_de_passe = mot_de_passe;
 	}
+	
 	public int getCredit() {
 		return 0;
 	}
+	
 	public void setCredit(int credit) {
 		this.credit = credit;
 	}
+	
 	public boolean isAdministrateur() {
 		return false;
 	}
+	
 	public void setAdministrateur(boolean administrateur) {
 		this.administrateur = administrateur;
 	}
